2a3bc9980c93a35495dd7bd460a78a98eda27202
dinh2644 <dinh2644.mail@gmail.com>
2024-07-18 09:37:23 -0400

    latest

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -175,7 +175,7 @@ const loginUser = async (req, res, next) => {
     // create token after successfully logging in
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       expires: new Date(Date.now() + 14*24*60*60*1000),
       secure: true,
       sameSite: 'none'
